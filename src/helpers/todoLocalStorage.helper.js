ca1438ca3b6addc228d9fcb24a6ceed91578c337
Vladimir Belmach <vbelmach97@gmail.com>
2023-05-12 11:34:28 +0200

    helpers folder created

---
+++ src/helpers/todoLocalStorage.helper.js
@@ -0,0 +1,18 @@
+import { getItemsFromLocalStorage } from "../repositories/localStorageRepository";
+import {
+  TODOS_LOCAL_STORAGE_NAME,
+  TODO_FILTER_ALL_BUTTON_NAME,
+} from "../constants";
+
+export const loadTodosStateFromLocalStorage = () => {
+  const storedTodos = getItemsFromLocalStorage(TODOS_LOCAL_STORAGE_NAME);
+  if (storedTodos) {
+    return {
+      todos: {
+        todos: JSON.parse(storedTodos),
+        filterValue: TODO_FILTER_ALL_BUTTON_NAME,
+      },
+    };
+  }
+  return undefined;
+};
--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,23 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todosSlice";
-import { getItemsFromLocalStorage } from "../repositories/localStorageRepository";
-import {
-  TODOS_LOCAL_STORAGE_NAME,
-  TODO_FILTER_ALL_BUTTON_NAME,
-} from "../constants";
-
-export const loadTodosStateFromLocalStorage = () => {
-  const storedTodos = getItemsFromLocalStorage(TODOS_LOCAL_STORAGE_NAME);
-  if (storedTodos) {
-    return {
-      todos: {
-        todos: JSON.parse(storedTodos),
-        filterValue: TODO_FILTER_ALL_BUTTON_NAME,
-      },
-    };
-  }
-  return undefined;
-};
+import { loadTodosStateFromLocalStorage } from "../helpers/todoLocalStorage.helper";
 
 const persistedState = loadTodosStateFromLocalStorage();
 
